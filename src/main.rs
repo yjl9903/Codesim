99b7795d792180ad63edd70c6a52a24c58adc095
XLor <yjl9903@vip.qq.com>
2021-11-23 01:56:55 +0800

    feat: read nm

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
 
-extern crate clang;
-
-mod parse;
-mod visitor;
-
-use parse::Parser;
-use std::path::PathBuf;
+use std::{
+  collections::HashSet,
+  path::{Path, PathBuf},
+};
 use structopt::StructOpt;
-use visitor::get_func_decl;
 
 #[derive(StructOpt)]
 #[structopt(name = "codesim", about = "Diff two single C++ code")]
@@ -27,54 +23,84 @@ struct CliOption {
 fn main() {
   let options = CliOption::from_args();
   if options.verbose {
-    println!("Diff: {:?} {:?}", options.code1, options.code2);
+    println!("Diff: {:?} v.s. {:?}", options.code1, options.code2);
   }
 
-  let parser = Parser::new();
+  let c1 = compile(&options.code1);
+  if options.verbose {
+    println!("Compile output of {:?} -> {:?}", &options.code1, &c1);
+  }
 
-  let tu = parser.parse(options.code1.into());
+  let c2 = compile(&options.code2);
+  if options.verbose {
+    println!("Compile output of {:?} -> {:?}", &options.code2, &c2);
+  }
 
-  let funcs = get_func_decl(&tu);
-  println!("Find {} functions", funcs.len());
-  for entity in funcs.iter() {
-    if let Some(name) = entity.get_name() {
-      println!("function: {:?}", name);
+  read_symtable(c1.path(), options.verbose);
+
+  read_symtable(c2.path(), options.verbose);
+}
+
+fn compile(file: &PathBuf) -> tempfile::NamedTempFile {
+  let temp = tempfile::NamedTempFile::new().unwrap();
+
+  std::process::Command::new("clang++")
+    .arg("--std=c++17")
+    .arg("-pedantic")
+    .arg("-O2")
+    .arg(file)
+    .arg("-o")
+    .arg(temp.path())
+    .output()
+    .expect("Failed to compile source file");
+
+  temp
+}
+
+lazy_static! {
+  static ref SYMTABLE_BLACKLIST: HashSet<&'static str> = {
+    let mut set = HashSet::new();
+    set.insert("__mh_execute_header");
+    set
+  };
+}
+
+fn read_symtable(file: &Path, verbose: bool) -> Vec<(String, String)> {
+  if verbose {
+    println!("nm {:?}", file);
+  }
+
+  let cmd = std::process::Command::new("nm")
+    .arg("--defined-only")
+    .arg("-g")
+    .arg("-P")
+    .arg("--demangle")
+    .arg("-S")
+    .arg(file)
+    .output()
+    .expect("Fail to run nm");
+
+  let content = String::from_utf8_lossy(&cmd.stdout).into_owned();
+
+  let mut symbols: Vec<(String, String)> = Vec::new();
+  for line in content.trim_end().split("\n") {
+    let line: Vec<&str> = if line.contains(" T ") {
+      line.trim_end().split(" T ").collect()
+    } else if line.contains(" t ") {
+      line.trim_end().split(" t ").collect()
+    } else {
+      continue;
+    };
+    let func = line.get(0).unwrap().to_string();
+    if SYMTABLE_BLACKLIST.contains(&func[..]) {
+      continue;
     }
-
-    entity.visit_children(|cur, _| {
-      println!("Visit: {:?}", cur);
-      clang::EntityVisitResult::Recurse
-    });
+    let address = line.get(1).unwrap().split(" ").next().unwrap().to_string();
+    if verbose {
+      println!("{} -> {}", &func, &address);
+    }
+    symbols.push((func, address));
   }
 
-  // // Get the structs in this translation unit
-  // let structs = tu
-  //   .get_entity()
-  //   .get_children()
-  //   .into_iter()
-  //   .filter(|e| e.get_kind() == clang::EntityKind::StructDecl)
-  //   .collect::<Vec<_>>();
-
-  // // Print information about the structs
-  // for struct_ in structs {
-  //   let type_ = struct_.get_type().unwrap();
-  //   let size = type_.get_sizeof().unwrap();
-  //   println!(
-  //     "struct: {:?} (size: {} bytes)",
-  //     struct_.get_name().unwrap(),
-  //     size
-  //   );
-
-  //   for field in struct_.get_children() {
-  //     if let Some(name) = field.get_name() {
-  //       if let Ok(offset) = type_.get_offsetof(&name) {
-  //         println!("  field: {:?} (offset: {} bits)", name, offset);
-  //       } else {
-  //         println!("  field: {:?}", name);
-  //       }
-  //     } else {
-
-  //     }
-  //   }
-  // }
+  symbols
 }
