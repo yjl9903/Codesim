cb0757d2886d1ccdb540bb446f10c3151a778cf6
XLor <yjl9903@vip.qq.com>
2021-11-23 15:04:19 +0800

    feat: dump func code

--- src/loader.rs
+++ src/loader.rs
@@ -50,7 +50,7 @@ impl Loader {
     }
   }
 
-  pub fn symbol_table(&self) -> Vec<(String, String)> {
+  pub fn symbol_table(&self) -> Vec<(String, u64)> {
     if self.verbose {
       println!("nm {:?}", self.source);
     }
@@ -66,7 +66,7 @@ impl Loader {
 
     let content = String::from_utf8_lossy(&cmd.stdout).into_owned();
 
-    let mut symbols: Vec<(String, String)> = Vec::new();
+    let mut symbols = Vec::new();
     for line in content.trim_end().split("\n") {
       let line: Vec<&str> = if line.contains(" T ") {
         line.trim_end().split(" T ").collect()
@@ -80,8 +80,9 @@ impl Loader {
         continue;
       }
       let address = line.get(1).unwrap().split(" ").next().unwrap().to_string();
+      let address = u64::from_str_radix(&address, 16).unwrap();
       if self.verbose {
-        println!("{} -> {}", &func, &address);
+        println!("Func: {} -> {}", &func, &address);
       }
       symbols.push((func, address));
     }
@@ -89,9 +90,67 @@ impl Loader {
     symbols
   }
 
-  pub fn dump(&self, _symbols: Vec<(String, String)>) -> HashMap<String, Vec<usize>> {
-    let map = HashMap::new();
-    // TODO
+  pub fn dump(&self, symbols: Vec<(String, u64)>) -> HashMap<u64, Vec<u64>> {
+    if self.verbose {
+      println!("objdump -d {:?}", self.source);
+    }
+
+    let func_address = symbols
+      .into_iter()
+      .map(|(func, addr)| (addr, func))
+      .collect::<HashMap<u64, String>>();
+
+    let cmd = std::process::Command::new("objdump")
+      .arg("-d")
+      .arg(self.elf_temp.path())
+      .output()
+      .expect("Fail to run objdump");
+
+    fn parse_address(line: &str) -> Option<u64> {
+      if let Some(address) = line.split(":").next() {
+        if let Ok(address) = u64::from_str_radix(address, 16) {
+          Some(address)
+        } else {
+          None
+        }
+      } else {
+        None
+      }
+    }
+
+    let mut map = HashMap::new();
+    let mut i = 0 as usize;
+    let content = String::from_utf8_lossy(&cmd.stdout).into_owned();
+    let content = content.trim().split("\n").collect::<Vec<&str>>();
+
+    while i < content.len() {
+      let line = content[i];
+      if let Some(address) = parse_address(line) {
+        if func_address.contains_key(&address) {
+          let mut code = Vec::new();
+          while i < content.len() {
+            if let Some(address) = parse_address(content[i]) {
+              code.push(address);
+              i = i + 1;
+            } else {
+              break;
+            }
+          }
+
+          if self.verbose {
+            let func = func_address.get(&address).unwrap();
+            println!("Found func: {:?} -> {:?} (size: {:?})", func, address, code.len());
+          }
+          
+          map.insert(address, code);
+        } else {
+          i = i + 1;
+        }
+      } else {
+        i = i + 1;
+      }
+    }
+
     map
   }
 }
